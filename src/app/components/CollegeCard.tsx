aa9153b88cea40c95106d0f5dae69032b919b3e9
Avery Budlong <averybudlong@gmail.com>
2024-07-11 21:34:16 -0700

    added search bar

---
+++ src/app/components/CollegeCard.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const CollegeCard = () => {
+    return (
+        <div>
+            testing yo
+        </div>
+    )
+}
+
+export default CollegeCard
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import CollegeSearch from '../app/components/CollegeSearch';
+import CollegeCard from '../app/components/CollegeCard';
 
 export default function Home() {
   return (
-    <main>
-      What the fuc kis up baby
+    <main className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">College Common Data Set Information</h1>
+      <CollegeSearch />
+
+      <h2>
+        <CollegeCard/>
+      </h2>
     </main>
   );
 }
