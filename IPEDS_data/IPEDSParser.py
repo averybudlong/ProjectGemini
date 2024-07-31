52597a78bd41a2731f82acf57e3e6f58c7fb1dc1
Avery Budlong <averybudlong@gmail.com>
2024-07-30 21:25:56 -0700

    added headcount and financial data from IPEDS.

--- IPEDS_data/IPEDSParser.py
+++ IPEDS_data/IPEDSParser.py
@@ -9,6 +9,8 @@ db = firestore.client()
 
 collegeData = pd.read_csv('hd2022.csv', encoding_errors='replace')
 cdsData = pd.read_csv('adm2022.csv')
+financialData = pd.read_csv('drvf2022.csv')
+headcountData = pd.read_csv('effy2022.csv')
 
 '''
 interface College {
@@ -50,6 +52,44 @@ interface College {
   ACTM75: number;
   imageUrl: string;
 }
+'''
+
+'''
+revenue: F1CORREV
+endowment per student: F1ENDMFT
+instruction per student: F1INSTFT
+research per student: F1RSRCFT
+
+EFFYALEV: 1=AllStudents, 2=AllUndergrad, 12=AllGrad, 21=TotalFullTime, 41=TotalPartTime
+EFYTOTLT: Total
+EFYTOTLM: Total men
+EFYTOTLW: Total women
+EFYAIANT: Total American Indian or Alaska Native
+EFYAIANM: American Indian or Alaska Native Men
+EFYAIANW: American Indian or Alaska Native Women
+EFYASIAT: Total Asian
+EFYASIAM: Asian Men
+EFYASIAW: Asian Women
+EFYBKAAT: Black Total
+EFYBKAAM: Black Men
+EFYBKAAW: Black Women
+EFYHISPT: Hispanic Total
+EFYHISPM: Hispanic Men
+EFYHISPW: Hispanic Women
+EFYNHPIT: Hawaiian / Pacific Islander Total
+EFYNHPIM: Hawaiian / Pacific Islander Men
+EFYNHPIW: Hawaiian / Pacific Islander Women
+EFYWHITT: White Total
+EFYWHITM: White Men
+EFYWHITW: White Women
+EFY2MORT: 2 or more races total
+EFY2MORM: 2 or more races men
+EFY2MORW: 2 or more races women
+EFYUNKNT: unknown total
+EFYNRALT: Non us resident total
+EFYNRALM: Non us resident Men
+EFYNRALW: Non us resident women
+EFYGUTOT: Other/unkown gender
 '''
 
 # maps firestore DB key to csv file key
@@ -95,6 +135,60 @@ attributeMapCDS = {
   'ACTM75': 'ACTMT75',
 }
 
+# EFFYALEV=2 for undergrad
+attributeMapHeadcountUndergrad = {
+  'enrolled': 'EFYTOTLT',
+  'enrolledM': 'EFYTOTLM',
+  'enrolledW': 'EFYTOTLW',
+  'americanIndian': 'EFYAIANT',
+  'americanIndianM': 'EFYAIANM',
+  'americanIndianW': 'EFYAIANW',
+  'asian': 'EFYASIAT',
+  'asianM': 'EFYASIAM',
+  'asianW': 'EFYASIAW',
+  'black': 'EFYBKAAT',
+  'blackM': 'EFYBKAAM',
+  'blackW': 'EFYBKAAW',
+  'hispanic': 'EFYHISPT',
+  'hispanicM': 'EFYHISPM',
+  'hispanicW': 'EFYHISPW',
+  'pacificIslander': 'EFYNHPIT',
+  'pacificIslanderM': 'EFYNHPIM',
+  'pacificIslanderW': 'EFYNHPIW',
+  'white': 'EFYWHITT',
+  'whiteM': 'EFYWHITM',
+  'whiteW': 'EFYWHITW',
+  'multipleRaces': 'EFY2MORT',
+  'multipleRacesM': 'EFY2MORM',
+  'multipleRacesW': 'EFY2MORW',
+  'unknownRace': 'EFYUNKNT',
+  'nonUS': 'EFYNRALT',
+  'nonUSM': 'EFYNRALM',
+  'nonUSW': 'EFYNRALW',
+  'unknownGender': 'EFYGUTOT',
+}
+
+# EFFYALEV=12 for grad
+attributeMapHeadcountGrad = {
+  'enrolledGrad': 'EFYTOTLT',
+  'enrolledGradM': 'EFYTOTLM',
+  'enrolledGradW': 'EFYTOTLW',
+  'nonUSGrad': 'EFYNRALT',
+  'nonUSGradM': 'EFYNRALM',
+  'nonUSGradW': 'EFYNRALW',
+}
+
+attributeMapFinancial = {
+  'revenuePublic': 'F1CORREV',
+  'revenuePrivate': 'F2CORREV',
+  'endowmentPerPublic': 'F1ENDMFT',
+  'endowmentPerPrivate': 'F2ENDMFT',
+  'instructionPerPublic': 'F1INSTFT',
+  'instructionPerPrivate': 'F2INSTFT',
+  'researchPerPublic': 'F1RSRCFT',
+  'researchPerPrivate': 'F2RSRCFT',
+}
+
 # adds data for each attribute in the dataMap to the currentCollege dictionary
 def processMap(dataMap, currentCollege, row):
   for attribute in dataMap:
@@ -103,18 +197,26 @@ def processMap(dataMap, currentCollege, row):
     if isinstance(value, pd.Series) and value.empty:
       currentCollege[attribute] = None
     elif isinstance(value, pd.Series):
-      currentCollege[attribute] = value.iloc[-1].item()
+      lastItem = value.iloc[-1]
+      if isinstance(lastItem, str):
+        currentCollege[attribute] = lastItem if lastItem != "." else ""
+      else:
+        currentCollege[attribute] = lastItem.item()
     elif isinstance(value, str):
       currentCollege[attribute] = value
     else:
       currentCollege[attribute] = value.item()
 
 # 163286 for UMD
-for i in range(20):
+# 166027 for Harvard
+for i in range(600, 800):
   currentCollege = {}
   collegeDataRow = collegeData.iloc[i]
   currentCollegeUID = collegeDataRow['UNITID'].item()
   cdsDataRow = cdsData.query(f'UNITID == {currentCollegeUID}')
+  headcountUndergradDataRow = headcountData.query(f'UNITID == {currentCollegeUID} and EFFYALEV == {2}')
+  headcountGradDataRow = headcountData.query(f'UNITID == {currentCollegeUID} and EFFYALEV == {12}')
+  financialDataRow = financialData.query(f'UNITID == {currentCollegeUID}')
 
   # If there is no data about number of applications the college wont be added to the database.
   if cdsDataRow['APPLCN'].empty:
@@ -122,12 +224,12 @@ for i in range(20):
 
   processMap(attributeMapCollege, currentCollege, collegeDataRow)
   processMap(attributeMapCDS, currentCollege, cdsDataRow)
+  processMap(attributeMapHeadcountUndergrad, currentCollege, headcountUndergradDataRow)
+  processMap(attributeMapHeadcountGrad, currentCollege, headcountGradDataRow)
+  processMap(attributeMapFinancial, currentCollege, financialDataRow)
 
   docRef = db.collection('testing').document(str(currentCollegeUID))
   docRef.set(currentCollege)
-
-
-
 
 
 
